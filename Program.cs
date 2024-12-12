22d9f8796499c8f93b618afda117f2c06b5c2d4d
Yohore Emmanuel <emmanuel.yohore@ynov.com>
2024-12-12 15:12:58 +0100

    all

--- Program.cs
+++ Program.cs
@@ -1,14 +1,1 @@
-﻿using System;
-
-namespace TPFinal
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      Car v = new Car(1,"Ford","2024","Disponible","Groupma-Loire-Bretagne"); 
-
-      Console.WriteLine(v.Marque);
-    }
-  }
-}
+﻿
