375fbe6df856091df19e8871cc391fd22cf1af75
Yohore Emmanuel <emmanuel.yohore@ynov.com>
2024-12-12 15:54:08 +0100

    update car

--- Car.cs
+++ Car.cs
@@ -2,27 +2,29 @@ class Car
 {
     public int Id ;
     public string Brand;
+    public string Model;
     public string Year;
     public string Statut;
     public string Assurance;
 
     public string LisenceTape;
-    public string Brand_car;
+    
 
-    public string valueMarque()
+    public string value_Brand_car()
     {
         return Brand;
     }
 
-    public Car(int Id ,string Brand, string Year, string Statut, string Assurance, string LisenceTape, string Brand_car)
+
+    public Car(int Id ,string Brand, string Model, string Year, string Statut, string Assurance, string LisenceTape)
     {
         this.Id = Id;
         this. Brand = Brand;
+        this.Model = Model;
         this.Year = Year;
         this.Statut = Statut;
         this.Assurance = Assurance;
         this.LisenceTape = LisenceTape;
-        this.Brand_car = Brand_car;
 
     }
 }
