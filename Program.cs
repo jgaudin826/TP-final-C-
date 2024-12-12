f5b04444a0e4825b748145049148126fd3abf9c1
Yohore Emmanuel <emmanuel.yohore@ynov.com>
2024-12-12 14:51:53 +0100

    creation d'une instance de car

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TPFinal
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Car v = new Car(1,"Ford","2024","Disponible","Groupma-Loire-Bretagne"); 
+
+      Console.WriteLine(v.Marque);
+    }
+  }
+}
