64b6ca35ef62a3f4d08728f8751f535babda0687
Yohore Emmanuel <emmanuel.yohore@ynov.com>
2024-12-16 13:31:50 +0100

    maj car

--- Car.cs
+++ Car.cs
@@ -3,7 +3,7 @@ class Car
     public int Id { get; set; }
     public string Brand { get; set; }
     public string Model { get; set; }
-    public string Year { get; set; }
+    public DateTime Year { get; set; }
     public bool Statut { get; set; }
     public string Assurance { get; set; }
 
@@ -15,15 +15,17 @@ class Car
         return $"Id : {Id}, Brand : {Brand}, Model : {Model}, Year : {Year}, Statut : {Statut}, Assurance : {Assurance}, LicensePlate : {LicensePlate}";
     }
   
-    public Car(int Id ,string Brand, string Model, string Year, bool Statut, string Assurance, string LicensePlate)
+    public Car(int Id ,string Brand, string Model, DateTime Year, bool Statut, string Assurance, string LicensePlate)
     {
         this.Id = Id;
         this. Brand = Brand;
         this.Model = Model;
         this.Year = Year;
-        this.Statut =true ;
+        this.Statut = Statut ;
         this.Assurance = Assurance;
         this.LicensePlate = LicensePlate;
 
     }
+
+     Car car1 = new Car(1,"Toyota","Corolla",new DateTime(2020, 1, 1),true,"Allianz","AB-123-CD");
 }
